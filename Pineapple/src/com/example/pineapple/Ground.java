08d68dcb895bbf8e9baaaf07b85b58fbdfbb6edb
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-04-11 16:47:01 +0200

    Added a Ground class

---
+++ Pineapple/src/com/example/pineapple/Ground.java
@@ -0,0 +1,13 @@
+package com.example.pineapple;
+
+public class Ground {
+	int[] x, y; //x- and y-values for the ground
+	int type;
+	
+	public Ground(int[] x, int[] y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+}
