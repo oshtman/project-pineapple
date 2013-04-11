914403ea224d9cb59f8495fdae4ae27eca8336b1
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-04-11 16:14:32 +0200

    Made a preliminary render method for protagonist

--- Pineapple/src/com/example/pineapple/GamePanel.java
+++ Pineapple/src/com/example/pineapple/GamePanel.java
@@ -3,17 +3,21 @@ package com.example.pineapple;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 	
 	private MainThread thread;
+	private Protagonist protagonist;
+	
 	
 	public GamePanel(Context context){
 		super(context);
 		getHolder().addCallback(this);
 		setFocusable(true);
+		protagonist = new Protagonist();
 		
 		thread = new MainThread(this.getHolder(), this);
 	}
@@ -26,8 +30,12 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 	//Method that gets called to render the graphics
 	public void render(Canvas canvas){
 		canvas.drawColor(Color.BLUE);
+		renderProtagonist(canvas);
 	}
 	
+	public void renderProtagonist(Canvas canvas){
+		canvas.drawRect((float)protagonist.getXPos(), (float)protagonist.getYPos(), (float)protagonist.getXPos()+30, (float)protagonist.getYPos()+30, new Paint());
+	}
 	
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
