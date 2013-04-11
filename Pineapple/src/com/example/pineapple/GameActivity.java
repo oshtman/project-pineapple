c6b974f24533f252fe911631eaff34bc2c592061
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-04-11 14:47:29 +0200

    Added the GameActivity class

--- Pineapple/src/com/example/pineapple/GameActivity.java
+++ Pineapple/src/com/example/pineapple/GameActivity.java
@@ -1,31 +1,22 @@
 package com.example.pineapple;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class GameActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_game);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.game, menu);
 		return true;
-	}
-	
-	public void startGame(View view){
-		
-		Intent intent = new Intent(this, GameActivity.class);
-		
-		startActivity(intent);
 	}
 
 }
