5f554ae7ce3d24d565c899a1ecb715125f22fb2a
Joar Lind <joarlind@hotmail.com>
2013-04-11 16:09:03 +0200

    Added some health to Protagonist

--- Pineapple/src/com/example/pineapple/Protagonist.java
+++ Pineapple/src/com/example/pineapple/Protagonist.java
@@ -1,91 +1,95 @@
-package com.example.pineapple;
-
-public class Protagonist {
-	
-	private double xPos;
-	private double yPos;
-	private double height;
-	private double length;
-//	private bitmap portrait;
-	private double xVel;
-	private double yVel;
-	private double xAcc;
-	private double yAcc;
-	private int health;
-
-	// CONSTRUCTOR
-	public Protagonist(double i,double j){
-		this.setXPos(i);
-		this.setYPos(j);
-		health = 100;
-	}
-	
-	public Protagonist(){
-		this.setXPos(75);
-		this.setYPos(25);
-		health = 100;
-	}
-	
-	// GET AND SET
-	public double getXPos() {
-		return xPos;
-	}
-	
-	public double getYPos() {
-		return yPos;
-	}
-	
-	public double getXVel() {
-		return xVel;
-	}
-	
-	public double getYVel() {
-		return yVel;
-	}
-	
-	public double getXAcc() {
-		return xAcc;
-	}
-	
-	public double getYAcc() {
-		return yAcc;
-	}
-	
-	public void setXPos(double n) {
-		xPos = n;
-	}
-	
-	public void setYPos(double n) {
-		yPos = n;
-	}
-	
-	public void setXVel(double n) {
-		xVel = n;
-	}
-	
-	public void setYVel(double n) {
-		yVel = n;
-	}
-	public void setXAcc(double n) {
-		xAcc = n;
-	}
-	
-	public void setYAcc(double n) {
-		yAcc = n;
-	}
-	
-	public void setAim() {
+		package com.example.pineapple;
 		
-	}
-	public double getAim() {
-		return 8;
-	}
-	
-	// ACTION
-	public void aim(){
+		public class Protagonist {
+			
+			private double xPos;
+			private double yPos;
+			private double height;
+			private double length;
+		//	private bitmap portrait;
+			private double xVel;
+			private double yVel;
+			private double xAcc;
+			private double yAcc;
+			private int health;
 		
-	}
-	public void fire(){
-		
-	}
-}
+			// CONSTRUCTOR
+			public Protagonist(double i,double j){
+				this.setXPos(i);
+				this.setYPos(j);
+				health = 100;
+			}
+			
+			public Protagonist(){
+				this.setXPos(75);
+				this.setYPos(25);
+				health = 100;
+			}
+			
+			// GET AND SET
+			public double getXPos() {
+				return xPos;
+			}
+			
+			public double getYPos() {
+				return yPos;
+			}
+			
+			public double getXVel() {
+				return xVel;
+			}
+			
+			public double getYVel() {
+				return yVel;
+			}
+			
+			public double getXAcc() {
+				return xAcc;
+			}
+			
+			public double getYAcc() {
+				return yAcc;
+			}
+			
+			public void setXPos(double n) {
+				xPos = n;
+			}
+			
+			public void setYPos(double n) {
+				yPos = n;
+			}
+			
+			public void setXVel(double n) {
+				xVel = n;
+			}
+			
+			public void setYVel(double n) {
+				yVel = n;
+			}
+			public void setXAcc(double n) {
+				xAcc = n;
+			}
+			
+			public void setYAcc(double n) {
+				yAcc = n;
+			}
+			
+			public void setAim() {
+				
+			}
+			public double getAim() {
+				return 8;
+			}
+			
+			// ACTION
+			public void aim(){
+				
+			}
+			public void fire(){
+				
+			}
+			public int reduceHealth(int n) {
+				health = n-1;
+				return health;
+			}
+		}
