aee82c75d1e8f63e32c062c837d4fe9f0797e4cc
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-04-11 16:56:24 +0200

    Added a getY method that interpolates from the given x

--- Pineapple/src/com/example/pineapple/Ground.java
+++ Pineapple/src/com/example/pineapple/Ground.java
@@ -9,5 +9,14 @@ public class Ground {
 		this.y = y;
 	}
 	
-	
+	public double getY(double x){
+		int index = 0;
+		while(this.x[index] < x){
+			index++;
+		}
+		
+		int diff = this.x[index] - this.x[index-1];
+		double percent = (x-this.x[index-1])/(double)diff;
+		return this.y[index-1]+percent*(this.y[index]-this.y[index-1]);
+	}
 }
