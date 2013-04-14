fe2c5d7d1c0ecbe8bfddde98ff1f407fecfc363f
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-04-14 13:51:36 +0200

    Created class Platform

---
+++ Pineapple/src/com/example/pineapple/Platform.java
@@ -0,0 +1,17 @@
+package com.example.pineapple;
+
+public class Platform {
+	private int[] upperX;
+	private int[] upperY;
+	private int[] lowerX;
+	private int[] lowerY;
+	
+	public Platform(int[] upperX, int[] upperY, int[] lowerX, int[] lowerY){
+		this.upperX = upperX;
+		this.upperY = upperY;
+		this.lowerX = lowerX;
+		this.lowerY = lowerY;
+	}
+	
+	
+}
