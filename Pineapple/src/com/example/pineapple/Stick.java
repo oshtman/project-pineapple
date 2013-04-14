6a346a9240b3e97faa98960524587640c61966c6
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-04-14 09:33:21 +0200

    Added methods to Stick for touchEvent handling

--- Pineapple/src/com/example/pineapple/Stick.java
+++ Pineapple/src/com/example/pineapple/Stick.java
@@ -5,6 +5,7 @@ public class Stick {
 	public final int RIGHT = 2;
 	private int x, y;
 	private int angle;
+	private int radius;
 	private boolean pointed;
 	
 	public Stick(int pos){
@@ -15,9 +16,26 @@ public class Stick {
 		}
 		y = 80;
 		pointed = false;
+		radius = 10;
 	}
 	
+	//Happens when the player touches the screen
+	public void handleTouch(int x, int y){
+		int dx = x - this.x;
+		int dy = y - this.y;
+		double dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+		if(dist <= radius){
+			//Calculate angle of the finger relative to the stick
+			angle = (int)(Math.atan2(dy, dx)*180/Math.PI);
+			//Set the sticks status to pointed
+			pointed = true;
+		}
+	}
 	
+	public void release(){
+		angle = 0;
+		pointed = false;
+	}
 	
 	
 }
