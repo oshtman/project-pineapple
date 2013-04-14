d26360fc1037da5bda5024caa4da24843c8942e2
Joar Lind <joarlind@hotmail.com>
2013-04-14 10:57:09 +0200

    Edit in jump, move, GamePlay

--- Pineapple/src/com/example/pineapple/GamePanel.java
+++ Pineapple/src/com/example/pineapple/GamePanel.java
@@ -38,7 +38,9 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 	
 	//Method that gets called every frame to update the games state
 	public void update(){
-		
+		if(leftStick.isPointed()) {
+			protagonist.move(leftStick.getAngle(), -5, 1, 3);
+		}
 	}
 	
 	//Method that gets called to render the graphics
