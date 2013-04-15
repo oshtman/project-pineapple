0f415745c1bdd3fbe47d4d87c68e9fa2db54e4c7
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-04-15 11:06:20 +0200

    Heavily optimized the LevelLoader to make it more understandable

--- Pineapple/src/com/example/pineapple/LevelLoader.java
+++ Pineapple/src/com/example/pineapple/LevelLoader.java
@@ -11,11 +11,7 @@ public class LevelLoader {
 	private ArrayList<int[]> yGroundArrays = new ArrayList<int[]>();
 	
 	//Lists that hold lists for the platforms for each level
-	private ArrayList<ArrayList<int[]>> pUX = new ArrayList<ArrayList<int[]>>();
-	private ArrayList<ArrayList<int[]>> pUY = new ArrayList<ArrayList<int[]>>();
-	private ArrayList<ArrayList<int[]>> pLX = new ArrayList<ArrayList<int[]>>();
-	private ArrayList<ArrayList<int[]>> pLY = new ArrayList<ArrayList<int[]>>();
-	
+	private ArrayList<ArrayList<int[][]>> platformArrays = new ArrayList<ArrayList<int[][]>>();
 	
 	//Put the ground arrays for all levels here!
 	private final int[] x1 = {-300, 0, 100, 150, 300};
@@ -27,30 +23,22 @@ public class LevelLoader {
 	
 	
 	//Level 1
-	private ArrayList<int[]> pUX1 = new ArrayList<int[]>();
-	private ArrayList<int[]> pUY1 = new ArrayList<int[]>();
-	private ArrayList<int[]> pLX1 = new ArrayList<int[]>();
-	private ArrayList<int[]> pLY1 = new ArrayList<int[]>();
+	private ArrayList<int[][]> platformsLevel1 = new ArrayList<int[][]>();
 	
-	//Variable syntax:
-	//pUX1_2 means the upper x array for platform 2 on level 1 
+	//Matrix syntax:
+	//p3_1 means platform 1 on level 3
 	//Platform 1
-	private final int[] pUX1_1 = {0, 10, 50};
-	private final int[] pUY1_1 = {20, 30, 30};
-	private final int[] pLX1_1 = {0, 10, 50};
-	private final int[] pLY1_1 = {20, 40, 30};
+	private final int[][] p1_1 = {
+	{0, 10, 50},
+	{20, 30, 30},
+	{0, 10, 50},
+	{20, 40, 30}};
 	
 	//Level 2
-	private ArrayList<int[]> pUX2 = new ArrayList<int[]>();
-	private ArrayList<int[]> pUY2 = new ArrayList<int[]>();
-	private ArrayList<int[]> pLX2 = new ArrayList<int[]>();
-	private ArrayList<int[]> pLY2 = new ArrayList<int[]>();
+	private ArrayList<int[][]> platform2 = new ArrayList<int[][]>();
 	
 	//Level 3
-	private ArrayList<int[]> pUX3 = new ArrayList<int[]>();
-	private ArrayList<int[]> pUY3 = new ArrayList<int[]>();
-	private ArrayList<int[]> pLX3 = new ArrayList<int[]>();
-	private ArrayList<int[]> pLY3 = new ArrayList<int[]>();
+	private ArrayList<int[][]> platform3 = new ArrayList<int[][]>();
 	
 	public LevelLoader(){
 		//level 2 "Valley of whale"
@@ -68,27 +56,10 @@ public class LevelLoader {
 		yGroundArrays.add(y2);
 		
 		//Add the platforms to their respective lists
-		pUX1.add(pUX1_1);
-		pUY1.add(pUY1_1);
-		pLX1.add(pLX1_1);
-		pLY1.add(pLY1_1);
+		platformsLevel1.add(p1_1);
 		
 		//Add the lists of each level to the list of lists
-		pUX.add(pUX1);
-		pUY.add(pUY1);
-		pLX.add(pLX1);
-		pLY.add(pLY1);
-		
-		pUX.add(pUX2);
-		pUY.add(pUY2);
-		pLX.add(pLX2);
-		pLY.add(pLY2);
-		
-		pUX.add(pUX3);
-		pUY.add(pUY3);
-		pLX.add(pLX3);
-		pLY.add(pLY3);
-		
+		platformArrays.add(platformsLevel1);
 	}
 	
 	public int[] getLevelX(int level){
@@ -100,16 +71,16 @@ public class LevelLoader {
 	}
 	
 	public int[] getPlatformUpperX(int level, int platform){
-		return pUX.get(level-1).get(platform-1);
+		return platformArrays.get(level-1).get(platform-1)[0];
 	}
 	public int[] getPlatformUpperY(int level, int platform){
-		return pUY.get(level-1).get(platform-1);
+		return platformArrays.get(level-1).get(platform-1)[1];
 	}
 	public int[] getPlatformLowerX(int level, int platform){
-		return pLX.get(level-1).get(platform-1);
+		return platformArrays.get(level-1).get(platform-1)[2];
 	}
 	public int[] getPlatformLowerY(int level, int platform){
-		return pLY.get(level-1).get(platform-1);
+		return platformArrays.get(level-1).get(platform-1)[3];
 	}
 	
 }
