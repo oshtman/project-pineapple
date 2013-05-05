18f2b2dcfca1493d64c21835c609531add13a476
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-05-05 17:37:06 +0200

    Modified MainActivity so that it detects screen width and height and scales the menu components accordingly

--- Pineapple/src/com/example/pineapple/MainActivity.java
+++ Pineapple/src/com/example/pineapple/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.pineapple;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class MainActivity extends BaseActivity {
-
+	
+	private final String TAG = MainActivity.class.getSimpleName();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+	
+		
+		
+		
+		scaleBitmaps();
+		
 	}
 
 	@Override
@@ -24,6 +42,21 @@ public class MainActivity extends BaseActivity {
 		Intent intent = new Intent(this, GameActivity.class);
 		
 		startActivity(intent);
+	}
+	
+	public void scaleBitmaps(){
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		int screenHeight = display.getHeight();
+		int screenWidth = display.getWidth();
+		Log.d(TAG, "Screen Width = " + screenWidth);
+		Log.d(TAG, "Screen Height = " + screenHeight);
+		
+		double scaleX = screenWidth/155.;
+		double scaleY = screenHeight/100.;
+		Bitmap playBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.bird), (int)(50*scaleX), (int)(50*scaleY), true);
+		ImageButton playButton = (ImageButton) findViewById(R.id.playButton);
+		playButton.setImageBitmap(playBitmap);
 	}
 
 }
