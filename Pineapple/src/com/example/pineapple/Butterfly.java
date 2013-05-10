6b056c5611eea65f14288b03e9a831e6f8979ca4
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-05-10 19:01:14 +0200

    Changed the movement of the butterfly

--- Pineapple/src/com/example/pineapple/Butterfly.java
+++ Pineapple/src/com/example/pineapple/Butterfly.java
@@ -15,7 +15,7 @@ public class Butterfly {
 		counter += Math.PI/180;
 		this.x = 70 + 30*Math.sin(counter) + 20*Math.sin(counter/2) + 20*Math.sin(counter/3) + 20*Math.sin(counter/4);
 		this.y = 40 + 10*Math.cos(counter) + 10*Math.cos(counter/2) + 10*Math.cos(counter/3) + 10*Math.cos(counter/4);
-		rot = (int)(30*Math.cos(counter) + 20*Math.cos(counter/2) + 20*Math.cos(counter/3) + 20*Math.cos(counter/4))/3;
+		rot = (int)(30*Math.cos(counter) + 10*Math.cos(counter/2) + 7*Math.cos(counter/3) + 5*Math.cos(counter/4))/3;
 		if(counter > 24*Math.PI){
 			counter = 0;
 		}
