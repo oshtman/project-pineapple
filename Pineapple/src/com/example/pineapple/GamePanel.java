76dbf4735b4dc541d4fa0be515930523983a5e82
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-05-11 19:04:51 +0200

    Working on LevelComplete layout

--- Pineapple/src/com/example/pineapple/GamePanel.java
+++ Pineapple/src/com/example/pineapple/GamePanel.java
@@ -25,6 +25,9 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 	private final String TAG = GamePanel.class.getSimpleName();
 	private final int INVALID_POINTER = -1;
 	public final static String LEVEL = "com.pineapple.GamePanel.LEVEL";
+	public final static String SCORE_KILL = "com.pineapple.GamePanel.SCORE_KILL";
+	public final static String TIME = "com.pineapple.GamePanel.TIME";
+	public final static String HEALTH = "com.pineapple.GamePanel.HEALTH";
 
 	private int leftStickId = INVALID_POINTER;
 	private int rightStickId = INVALID_POINTER;
@@ -535,6 +538,9 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 				Context context = getContext();
 				Intent intent = new Intent(context, LevelCompleteActivity.class);
 				intent.putExtra(LEVEL, level);
+				intent.putExtra(SCORE_KILL, scoreKill);
+				intent.putExtra(TIME, time);
+				intent.putExtra(HEALTH, (int)(protagonist.getHealth()*100));
 				context.startActivity(intent);
 				if(viewStatistics){
 					playTimeTotal = MainThread.updateInterval*time/1000;
@@ -1086,7 +1092,6 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 		for(i = 0; i < flowers.size(); i++){
 			if(flowers.get(i)[0] > screenX - Const.flowerSize/2 && flowers.get(i)[0] < screenX + width + Const.flowerSize/2){
 				canvas.drawBitmap(flowerBitmap, (float)((flowers.get(i)[0] - Const.flowerSize/4 - screenX)*scaleX), (float)((ground.getYFromX(flowers.get(i)[0])-Const.flowerSize-screenY)*scaleY), null);
-
 			}
 		}
 	}
@@ -1196,8 +1201,6 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
 	//Draw Hints
 	public void renderTime(Canvas canvas){
-
-
 		int secs = (int)(time*MainThread.updateInterval/1000);
 		int mins = secs/60;
 		secs =     secs%60;
--- Pineapple/src/com/example/pineapple/LevelCompleteActivity.java
+++ Pineapple/src/com/example/pineapple/LevelCompleteActivity.java
@@ -1,32 +1,72 @@
 package com.example.pineapple;
 
-import android.os.Bundle;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LevelCompleteActivity extends BaseActivity {
 
 	private final String TAG = LevelCompleteActivity.class.getSimpleName();
+	private boolean newLevel;
+	private int completedLevel;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_level_complete);
+		
 		SharedPreferences settings = this.getSharedPreferences("gameSettings", Context.MODE_PRIVATE);
 		int currentLevel = settings.getInt("currentLevel", 0);
-		int completedLevel = getIntent().getExtras().getInt(GamePanel.LEVEL);
+		completedLevel = getIntent().getExtras().getInt(GamePanel.LEVEL);
+		int[] scoreKill = getIntent().getExtras().getIntArray(GamePanel.SCORE_KILL);
+		int time = getIntent().getExtras().getInt(GamePanel.TIME);
+		int health = getIntent().getExtras().getInt(GamePanel.HEALTH);
 		
 		if(completedLevel == currentLevel){
+			newLevel = true;
 			Editor e = settings.edit();
 			e.putInt("currentLevel", completedLevel+1);
 			e.commit();
-			Log.d(TAG, "You unlocked a new level!");
 		}
+		
+		if(true){
+			TextView dataText;
+			dataText = (TextView) findViewById(R.id.normalsKilledText);
+			dataText.setText(""+scoreKill[0]);
+			dataText = (TextView) findViewById(R.id.ninjasKilledText);
+			dataText.setText(""+scoreKill[1]);
+			dataText = (TextView) findViewById(R.id.tanksKilledText);
+			dataText.setText(""+scoreKill[2]);
+			
+			int milliSecs = time*MainThread.updateInterval;
+			String mins = ((milliSecs/1000.)/60 >= 10)?""+(int)(milliSecs/1000.)/60:"0"+(int)(milliSecs/1000.)/60;
+			String secs = ((milliSecs/1000.)%60 >= 10)?""+(int)(milliSecs/1000.)%60:"0"+(int)(milliSecs/1000.)%60;
+			String millisecs = (milliSecs%1000 < 10)?"00"+(milliSecs%1000):(milliSecs%1000 < 100)?"0"+(milliSecs%1000):""+(milliSecs%1000);
+			
+			dataText = (TextView) findViewById(R.id.timeText);
+			dataText.setText(mins+":"+secs+","+millisecs);
+			
+			dataText = (TextView) findViewById(R.id.healthText);
+			dataText.setText(health + "%");
+		}
+		TextView newLevelText = (TextView) findViewById(R.id.newLevelText);
+		
+		if(newLevel){
+			Log.d(TAG, "You unlocked a new level!");
+			newLevelText.setVisibility(TextView.VISIBLE);
+		} else {
+			newLevelText.setVisibility(TextView.INVISIBLE);
+		}
+		
 	}
 
 	@Override
@@ -38,6 +78,18 @@ public class LevelCompleteActivity extends BaseActivity {
 
 	public void goToMain(View view){
 		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+	
+	public void goToSameLevel(View view){
+		Intent intent = new Intent(this, GameActivity.class);
+		intent.putExtra(MenuPanel.LEVEL, completedLevel);
+		startActivity(intent);
+	}
+	
+	public void goToNextLevel(View view){
+		Intent intent = new Intent(this, GameActivity.class);
+		intent.putExtra(MenuPanel.LEVEL, completedLevel+1);
 		startActivity(intent);
 	}
 	
