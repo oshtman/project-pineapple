33debe19260d67990af157bd53cab513280b1753
Oskar Wåglund <oskar.waglund@hotmail.com>
2013-05-14 14:04:30 +0200

    Balanced the HeatMeter

--- Pineapple/src/com/example/pineapple/MainActivity.java
+++ Pineapple/src/com/example/pineapple/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends BaseActivity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		menuPanel = new MenuPanel(this);
-
 		setContentView(menuPanel);
 	}
 
