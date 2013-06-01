bcc87bc5468884c5992bc2046b9f8e97a8202c74
Joar Lind <joarlind@hotmail.com>
2013-06-01 14:51:59 +0200

    Papilian*

--- Pineapple/src/com/pineapple/valentine/LevelLoader.java
+++ Pineapple/src/com/pineapple/valentine/LevelLoader.java
@@ -967,8 +967,8 @@ public class LevelLoader {
 			//Birds
 			birds.add(new Bird(2330, -340 - 43, true));
 			//Hints
-			hints.add(new Hint(50, 0, "Oh a butterfly! I love butterflies! There's one more. And there! Over there, on the flower! What a nice flower, I can count to SIX of them. Maybe I'll take one with me when I go back home. Oh well, I should start climbing.. //A happy Papilio"));
-			hints.add(new Hint(2000, 0, "Aah!! I did climb all of them... and counted to nine! NINE!! My legs are killing me! //A tired Papilio"));
+			hints.add(new Hint(50, 0, "Oh a butterfly! I love butterflies! There's one more. And there! Over there, on the flower! What a nice flower, I can count to SIX of them. Maybe I'll take one with me when I go back home. Oh well, I should start climbing.. //A happy Papilian"));
+			hints.add(new Hint(2000, 0, "Aah!! I did climb all of them... and counted to nine! NINE!! My legs are killing me! //A tired Papilian"));
 			break;
 		case 10:
 			//Start and finish
